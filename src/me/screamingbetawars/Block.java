e0bb5cf7c6c783c15368aa59cfb13b4c4c0b24b2
Lort533 <76491869+Lort533@users.noreply.github.com>
2022-04-02 21:34:43 +0200

    eliminate disk abuse

--- src/me/screamingbetawars/Block.java
+++ src/me/screamingbetawars/Block.java
@@ -3,12 +3,10 @@ package me.screamingbetawars;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.*;
 import me.screamingbetawars.ConfigManager.*;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,64 +27,56 @@ public class Block extends BlockListener implements Listener {
     }
 
     public void onBlockBreak(BlockBreakEvent event) {
-        File[] list = Game.game.getMaps();
-        if (list != null) {
-            for (File yaml_file : list) {
-                if (!yaml_file.isDirectory()) {
-                    if (cfg.check(yaml_file.getName().replace(".yml", ""), true, false).equals("true")) {
-                        Location pos1 = cfg.getLocation(yaml_file.getName().replace(".yml", ""), "pos1");
-                        Location pos2 = cfg.getLocation(yaml_file.getName().replace(".yml", ""), "pos2");
-                        if(pos.check(pos1, pos2, event.getBlock().getLocation()) == 3) {
-                            if (Game.joined_players.get(event.getPlayer().getName()).equals(yaml_file.getName().replace(".yml", ""))) {
-                                ArrayList<Location> locations = new ArrayList<>();
-                                for(Map<String, Object> entry : Game.game.getTeams(Game.joined_players.get(event.getPlayer().getName())).values()) {
-                                    locations.add((Location) entry.get("bed1"));
-                                    locations.add((Location) entry.get("bed2"));
-                                }
-                                boolean check = false;
-                                for(Location bed : locations) {
-                                    if(bed.distance(event.getBlock().getLocation()) == 0) {
-                                        check = true;
-                                        for(Map.Entry<String, Map<String, Object>> entry : Game.game.getTeams(Game.joined_players.get(event.getPlayer().getName())).entrySet()) {
-                                            if((event.getBlock().getLocation().getBlockX() == ((Location) entry.getValue().get("bed1")).getBlockX()) && (event.getBlock().getLocation().getBlockY() == ((Location) entry.getValue().get("bed1")).getBlockY()) && (event.getBlock().getLocation().getBlockZ() == ((Location) entry.getValue().get("bed1")).getBlockZ())) {
-                                                if(Game.destroyed_beds.get(entry.getKey()) != null) return;
-                                                Game.destroyed_beds.put(entry.getKey(), yaml_file.getName().replace(".yml", ""));
-                                                Map<String, String> list2 = Game.game.getWithValue(Game.joined_players, yaml_file.getName().replace(".yml", ""));
-                                                for(String player : list2.keySet()) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Player " + event.getPlayer().getName() + " has destroyed the bed of team \"" + entry.getKey() + "\".");
-                                            } else if((event.getBlock().getLocation().getBlockX() == ((Location) entry.getValue().get("bed2")).getBlockX()) && (event.getBlock().getLocation().getBlockY() == ((Location) entry.getValue().get("bed2")).getBlockY()) && (event.getBlock().getLocation().getBlockZ() == ((Location) entry.getValue().get("bed2")).getBlockZ())) {
-                                                if(Game.destroyed_beds.get(entry.getKey()) != null) return;
-                                                Game.destroyed_beds.put(entry.getKey(), yaml_file.getName().replace(".yml", ""));
-                                                Map<String, String> list2 = Game.game.getWithValue(Game.joined_players, yaml_file.getName().replace(".yml", ""));
-                                                for(String player : list2.keySet()) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Player " + event.getPlayer().getName() + " has destroyed the bed of team \"" + entry.getKey() + "\".");
-                                            }
-                                        }
-                                        break;
+        for (String map : ConfigManager.map_cache.keySet()) {
+            if (cfg.check(map, true, false).equals("true")) {
+                Location pos1 = cfg.getLocation(map, "pos1");
+                Location pos2 = cfg.getLocation(map, "pos2");
+                if(pos.check(pos1, pos2, event.getBlock().getLocation()) == 3) {
+                    if (Game.joined_players.get(event.getPlayer().getName()).equals(map)) {
+                        ArrayList<Location> locations = new ArrayList<>();
+                        for(Map<String, Object> entry : Game.game.getTeams(Game.joined_players.get(event.getPlayer().getName())).values()) {
+                            locations.add((Location) entry.get("bed1"));
+                            locations.add((Location) entry.get("bed2"));
+                        }
+                        boolean check = false;
+                        for(Location bed : locations) {
+                            if(bed.distance(event.getBlock().getLocation()) == 0) {
+                                check = true;
+                                for(Map.Entry<String, Map<String, Object>> entry : Game.game.getTeams(Game.joined_players.get(event.getPlayer().getName())).entrySet()) {
+                                    if((event.getBlock().getLocation().getBlockX() == ((Location) entry.getValue().get("bed1")).getBlockX()) && (event.getBlock().getLocation().getBlockY() == ((Location) entry.getValue().get("bed1")).getBlockY()) && (event.getBlock().getLocation().getBlockZ() == ((Location) entry.getValue().get("bed1")).getBlockZ())) {
+                                        if(Game.destroyed_beds.get(entry.getKey()) != null) return;
+                                        Game.destroyed_beds.put(entry.getKey(), map);
+                                        Map<String, String> list2 = Game.game.getWithValue(Game.joined_players, map);
+                                        for(String player : list2.keySet()) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Player " + event.getPlayer().getName() + " has destroyed the bed of team \"" + entry.getKey() + "\".");
+                                    } else if((event.getBlock().getLocation().getBlockX() == ((Location) entry.getValue().get("bed2")).getBlockX()) && (event.getBlock().getLocation().getBlockY() == ((Location) entry.getValue().get("bed2")).getBlockY()) && (event.getBlock().getLocation().getBlockZ() == ((Location) entry.getValue().get("bed2")).getBlockZ())) {
+                                        if(Game.destroyed_beds.get(entry.getKey()) != null) return;
+                                        Game.destroyed_beds.put(entry.getKey(), map);
+                                        Map<String, String> list2 = Game.game.getWithValue(Game.joined_players, map);
+                                        for(String player : list2.keySet()) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Player " + event.getPlayer().getName() + " has destroyed the bed of team \"" + entry.getKey() + "\".");
                                     }
                                 }
-                                if(blocks.containsValue(event.getBlock().getLocation())) {
-                                    blocks.remove(yaml_file.getName().replace(".yml", ""), event.getBlock().getLocation());
-                                } else if(!check) event.setCancelled(true);
-                            } else event.setCancelled(true);
+                                break;
+                            }
                         }
-                    }
+                        if(blocks.containsValue(event.getBlock().getLocation())) {
+                            blocks.remove(map, event.getBlock().getLocation());
+                        } else if(!check) event.setCancelled(true);
+                    } else event.setCancelled(true);
                 }
             }
         }
     }
-    //TODO villagers
+
     public void onBlockPlace(BlockPlaceEvent event) {
-        File[] list = Game.game.getMaps();
-        if (list != null) {
-            for (File yaml_file : list) {
-                if ((!yaml_file.isDirectory()) && (!yaml_file.getName().equals("config.yml"))) {
-                    if (cfg.check(yaml_file.getName().replace(".yml", ""), true, false).equals("true")) {
-                        Location pos1 = cfg.getLocation(yaml_file.getName().replace(".yml", ""), "pos1");
-                        Location pos2 = cfg.getLocation(yaml_file.getName().replace(".yml", ""), "pos2");
-                        if(pos.check(pos1, pos2, event.getBlock().getLocation()) == 3) {
-                            if (Game.joined_players.get(event.getPlayer().getName()).equals(yaml_file.getName().replace(".yml", ""))) {
-                                blocks.put(yaml_file.getName().replace(".yml", ""), event.getBlock().getLocation());
-                            } else event.setCancelled(true);
-                        }
+        for (String map : ConfigManager.map_cache.keySet()) {
+            if (!map.equals("config.yml")) {
+                if (cfg.check(map, true, false).equals("true")) {
+                    Location pos1 = cfg.getLocation(map, "pos1");
+                    Location pos2 = cfg.getLocation(map, "pos2");
+                    if(pos.check(pos1, pos2, event.getBlock().getLocation()) == 3) {
+                        if (Game.joined_players.get(event.getPlayer().getName()).equals(map)) {
+                            blocks.put(map, event.getBlock().getLocation());
+                        } else event.setCancelled(true);
                     }
                 }
             }
@@ -94,16 +84,11 @@ public class Block extends BlockListener implements Listener {
     }
 
     public void onBlockBurn(BlockBurnEvent event) {
-        File[] list = Game.game.getMaps();
-        if (list != null) {
-            for (File yaml_file : list) {
-                if (!yaml_file.isDirectory()) {
-                    if(cfg.check(yaml_file.getName().replace(".yml", ""), true, false).equals("true")) {
-                        Location pos1 = cfg.getLocation(yaml_file.getName().replace(".yml", ""), "pos1");
-                        Location pos2 = cfg.getLocation(yaml_file.getName().replace(".yml", ""), "pos2");
-                        if (pos.check(pos1, pos2, event.getBlock().getLocation()) == 3) event.setCancelled(true);
-                    }
-                }
+        for (String map : ConfigManager.map_cache.keySet()) {
+            if(cfg.check(map, true, false).equals("true")) {
+                Location pos1 = cfg.getLocation(map, "pos1");
+                Location pos2 = cfg.getLocation(map, "pos2");
+                if (pos.check(pos1, pos2, event.getBlock().getLocation()) == 3) event.setCancelled(true);
             }
         }
     }
