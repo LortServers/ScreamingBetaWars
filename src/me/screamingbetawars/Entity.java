3803332918e6cf00578e2375e820d2d665d30556
Lort533 <76491869+Lort533@users.noreply.github.com>
2022-09-14 00:20:26 +0200

    Shop now reads from a file - thank you https://stackoverflow.com/a/2013172, https://stackoverflow.com/a/14865929, https://regex101.com/ and https://wiki.vg/

--- src/me/screamingbetawars/Entity.java
+++ src/me/screamingbetawars/Entity.java
@@ -2,6 +2,7 @@ package me.screamingbetawars;
 
 import net.minecraft.server.*;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
@@ -9,30 +10,50 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerListener;
 import me.screamingbetawars.Main.EventHandler;
 
-import java.util.EventListener;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Entity extends PlayerListener implements Listener, EventListener {
+public class Entity extends PlayerListener implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEntityEvent event) {
         if(Game.joined_players.containsKey(event.getPlayer().getName())) {
             if(Game.npcs.containsKey(event.getRightClicked().getEntityId())) openShop(event.getPlayer());
         }
     }
+
+    // Packet102WindowClick - possible way of receiving clicked item slots
+
     public static void openShop(Player player) {
         try {
-            Packet100OpenWindow test = new Packet100OpenWindow();
-            test.a = 100;
-            test.b = 0;
-            test.c = "BedWars Shop";
-            test.d = 9;
-            ((CraftPlayer) player).getHandle().netServerHandler.sendPacket(test);
-            Packet103SetSlot test2 = new Packet103SetSlot();
-            test2.a = test.a;
-            test2.b = 0;
-            ItemStack item = new ItemStack(Item.BOW);
-            item.count = 1;
-            test2.c = item;
-            ((CraftPlayer) player).getHandle().netServerHandler.sendPacket(test2);
+            Packet100OpenWindow shop = new Packet100OpenWindow();
+            shop.a = 100;
+            shop.b = 0;
+            shop.c = "BedWars Shop";
+            shop.d = 27;
+            ((CraftPlayer) player).getHandle().netServerHandler.sendPacket(shop);
+            for(int i = 1; i<=9; i++) {
+                String data = ConfigManager.cfg.get("shop_file", "slot-" + i);
+                Matcher matcher = Pattern.compile("(.+?);").matcher(data);
+                ArrayList<String> data_list = new ArrayList<>();
+                while(matcher.find()) {
+                    data_list.add(matcher.group(1));
+                }
+                Packet103SetSlot packet1 = new Packet103SetSlot();
+                packet1.a = shop.a;
+                packet1.b = i-1;
+                ItemStack item1 = new ItemStack(Item.byId[Material.valueOf(data_list.get(0)).getId()]);
+                item1.count = Integer.parseInt(data_list.get(1));
+                packet1.c = item1;
+                ((CraftPlayer) player).getHandle().netServerHandler.sendPacket(packet1);
+                Packet103SetSlot packet2 = new Packet103SetSlot();
+                packet2.a = shop.a;
+                packet2.b = i+17;
+                ItemStack item2 = new ItemStack(Item.byId[Material.valueOf(data_list.get(2)).getId()]);
+                item2.count = Integer.parseInt(data_list.get(3));
+                packet2.c = item2;
+                ((CraftPlayer) player).getHandle().netServerHandler.sendPacket(packet2);
+            }
         } catch(NullPointerException e) { player.sendMessage(e.getStackTrace()[0].toString()); }
         player.sendMessage(ChatColor.GOLD + "You've opened the shop!");
     }
