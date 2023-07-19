4cd0e16d01ede7f32ee30c6c6d598bceecc173d6
Lort533 <76491869+Lort533@users.noreply.github.com>
2023-07-19 03:34:13 +0200

    Made code perhaps a bit more readable

--- src/me/screamingbetawars/Game.java
+++ src/me/screamingbetawars/Game.java
@@ -13,31 +13,36 @@ import org.bukkit.entity.Sheep;
 import org.bukkit.inventory.ItemStack;
 
 public class Game {
-    static HashMap<String, Boolean> games = new HashMap<>();
-    public static HashMap<String, String> joined_players = new HashMap<>();
-    static HashMap<String, Map<String, String>> teams = new HashMap<>();
-    static HashMap<Material, Location> beds = new HashMap<>();
-    public static HashMap<Integer, Map.Entry<String, Location>> npcs = new HashMap<>();
-    public static HashMap<String, String> destroyed_beds = new HashMap<>();
-    public static HashMap<Integer, String> task_ids = new HashMap<>();
+    static class BWGame {
+        public static boolean started = false;
+        public static ArrayList<String> joined_players = new ArrayList<>();
+        static HashMap<String, String> teams = new HashMap<>();
+        static HashMap<Material, Location> beds = new HashMap<>();
+        public static HashMap<Integer, Location> npcs = new HashMap<>();
+        public static ArrayList<String> destroyed_beds = new ArrayList<>();
+        public static ArrayList<Integer> task_ids = new ArrayList<>();
+    }
+    static HashMap<String, BWGame> games = new HashMap<>();
     public static class game {
+        public static BWGame getGame(String game) {
+            if(games.get(game) == null) games.put(game, new BWGame());
+            return games.get(game);
+        }
         public static String joinGame(String game, String nick) {
-            if (!joined_players.containsKey(nick)) {
-                if (!games.containsKey(game)) games.put(game, false);
-                if (!teams.containsKey(game)) teams.put(game, new HashMap<>());
-                if(games.get(game).equals(false)) joined_players.put(nick, game);
+            BWGame game2 = getGame(game);
+            if (!game2.joined_players.contains(nick)) {
+                if(!game2.started) game2.joined_players.add(nick);
                 else return ChatColor.RED + "This game has already started!";
             } else return ChatColor.RED + "You are already in a game!";
             Bukkit.getPlayer(nick).teleport(cfg.getLocation(game, "lobby-"));
             Bukkit.getPlayer(nick).sendMessage(ChatColor.AQUA + "You've successfully joined a map!");
             int size = 0;
             for(Map<String, Object> team : getTeams(game).values()) size += Integer.parseInt(String.valueOf(team.get("size")));
-            Map<String, String> list = getWithValue(joined_players, game);
-            if(list.size() == 1) proceedGame(game);
-            for(String player : list.keySet()) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Player " + nick + " has joined the map (" + list.size() + "/" + size + ")!");
+            if(game2.joined_players.size() == 1) proceedGame(game);
+            for(String player : game2.joined_players) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Player " + nick + " has joined the map (" + game2.joined_players.size() + "/" + size + ")!");
             Map<String, Map<String, Object>> game_teams = new HashMap<>(getTeams(game));
             for (Map.Entry<String, Map<String, Object>> entry : getTeams(game).entrySet()) {
-                if(Integer.parseInt((String) entry.getValue().get("size")) == countWithValue(joined_players, entry.getKey())) game_teams.remove(entry.getKey(), entry.getValue());
+                if(Integer.parseInt((String) entry.getValue().get("size")) == game2.joined_players.size()) game_teams.remove(entry.getKey(), entry.getValue());
             }
             String teams = "";
             boolean check = false;
@@ -114,54 +119,53 @@ public class Game {
         }
 
         public static String joinTeam(String nick, String team) {
-            if (joined_players.containsKey(nick)) {
-                String game = joined_players.get(nick);
+            String game = getPlayerMap(nick);
+            BWGame game2 = getGame(game);
+            if(game2.joined_players.contains(nick)) {
                 if (getTeams(game).containsKey(team)) {
                     int count = 0;
-                    for (Map.Entry<String, String> team_count : teams.get(game).entrySet()) {
+                    for (Map.Entry<String, String> team_count : game2.teams.entrySet()) {
                         if (team_count.getKey().equals(team)) count++;
                     }
                     if (count != Integer.parseInt((String) getTeams(game).get(team).get("size"))) {
-                        Map<String, String> data = new HashMap<>(teams.get(game));
-                        data.put(nick, team);
-                        leaveTeam(nick);
-                        teams.put(game, data);
+                        game2.teams.put(nick, team);
+                        //leaveTeam(nick);
                         return ChatColor.AQUA + "Successfully joined team \"" + team + "\"!";
                     } else return ChatColor.RED + "Team \"" + team + "\" is full!";
                 } else return ChatColor.RED + "Given team does not exist!";
             } else return ChatColor.RED + "You are not playing on any map!";
         }
 
-        public static String getPlayerTeam(String game, String nick) {
-            return teams.get(game).getOrDefault(nick, null);
+        public static String getPlayerTeam(String nick) {
+            return getGame(getPlayerMap(nick)).teams.getOrDefault(nick, null);
         }
 
         public static String leaveTeam(String nick) {
-            String game = joined_players.get(nick);
-            if(teams.get(game).containsKey(nick)) {
-                teams.get(game).remove(nick);
+            String game = getPlayerMap(nick);
+            BWGame game2 = getGame(game);
+            if(game2.teams.containsKey(nick)) {
+                game2.teams.remove(nick);
                 return ChatColor.AQUA + "Successfully left the team!";
             } else return ChatColor.RED + "You don't belong to any team!";
         }
 
         public static String leaveGame(String nick) {
-            if (!joined_players.containsKey(nick)) return ChatColor.RED + "You are not playing!";
-            String game = joined_players.get(nick);
-            joined_players.remove(nick);
-            teams.get(game).remove(nick);
+            String game = getPlayerMap(nick);
+            BWGame game2 = getGame(game);
+            if (!game2.joined_players.contains(nick)) return ChatColor.RED + "You are not playing!";
+            game2.joined_players.remove(nick);
+            game2.teams.remove(nick);
             int size = 0;
             for(Map<String, Object> team : getTeams(game).values()) size += Integer.parseInt(String.valueOf(team.get("size")));
-            Map<String, String> list = getWithValue(joined_players, game);
-            for(String player : list.keySet()) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Player " + nick + " has left the map (" + list.size() + "/" + size + ")!");
+            for(String player : game2.joined_players) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Player " + nick + " has left the map (" + game2.joined_players.size() + "/" + size + ")!");
             String random_team = "";
-            if(getWithValue(joined_players, game).size() > 0) {
-                for (Map.Entry<String, String> team : teams.get(game).entrySet()) {
-                    random_team = team.getKey();
+            if(game2.joined_players.size() > 0) {
+                for (String team : game2.joined_players) {
+                    random_team = team;
                     break;
                 }
-                if (getWithValue(joined_players, game).size() == getWithValue(teams.get(game), random_team).size()) {
-                    Map<String, String> list2 = getWithValue(joined_players, game);
-                    for (String player : list2.keySet()) {
+                if(game2.joined_players.size() == getWithValue(game2.teams, random_team).size()) {
+                    for (String player : game2.joined_players) {
                         Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Team " + random_team + " has won!");
                         endGame(game);
                     }
@@ -177,6 +181,7 @@ public class Game {
         }
 
         public static void proceedGame(String game) {
+            BWGame game2 = getGame(game);
             int size = 0;
             for(Map<String, Object> team : getTeams(game).values()) size += Integer.valueOf((String) team.get("size"));
             AtomicBoolean start = new AtomicBoolean(false);
@@ -184,18 +189,17 @@ public class Game {
             int finalSize = size;
             Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(new Main(), new Runnable() {
                 public void run() {
-                    Map<String, String> list = getWithValue(joined_players, game);
-                    if(list.size() > finalSize/2) {
+                    if(game2.joined_players.size() > finalSize/2) {
                         if(count.get() == 0) {
-                            for(String player : list.keySet()) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Game has been started!");
+                            for(String player : game2.joined_players) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Game has been started!");
                             startGame(game);
                             return;
                         }
-                        for(String player : list.keySet()) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Game starts in " + count.get() + " seconds!");
+                        for(String player : game2.joined_players) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Game starts in " + count.get() + " seconds!");
                         count.set(count.get()-1);
                         start.set(true);
                     } else if(start.get()) {
-                        for(String player : list.keySet()) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Not enough players, counting has been paused.");
+                        for(String player : game2.joined_players) Bukkit.getPlayer(player).sendMessage(ChatColor.AQUA + "Not enough players, counting has been paused.");
                         count.set(10);
                         start.set(false);
                     }
@@ -204,9 +208,10 @@ public class Game {
         }
 
         public static String forceStart(String nick) {
-            String game = joined_players.get(nick);
-            if(game != null) {
-                if((getWithValue(joined_players, game).size() > 1) || (Objects.equals(cfg.get("config", "debug"), "true"))) {
+            String game = getPlayerMap(nick);
+            BWGame game2 = getGame(game);
+            if(game2 != null) {
+                if((game2.joined_players.size() > 1) || (Objects.equals(cfg.get("config", "debug"), "true"))) {
                     startGame(game);
                     return ChatColor.AQUA + "Game has been started successfully!";
                 } else return ChatColor.RED + "You need at least two players to play!";
@@ -214,21 +219,23 @@ public class Game {
         }
 
         public static void startGame(String game) {
-            games.replace(game, true);
+            BWGame game2 = getGame(game);
+            game2.started = true;
             Map<String, Map<String, Object>> map_teams = getTeams(game);
-            if(teams.get(game).size() != getWithValue(joined_players, game).size()) {
+            if(game2.teams.size() != game2.joined_players.size()) {
                 Map<String, String> game_teams = new HashMap<>();
-                for (Map.Entry<String, Map<String, Object>> entry : map_teams.entrySet()) {
+                for(Map.Entry<String, Map<String, Object>> entry : map_teams.entrySet()) {
                     game_teams.put(entry.getKey(), String.valueOf(entry.getValue().get("size")));
                 }
-                for(String player : getWithValue(joined_players, game).keySet()) {
-                    if(!teams.get(game).containsKey(player)) {
+                for(String player : game2.joined_players) {
+                    if(!game2.teams.containsKey(player)) {
+                        Bukkit.getLogger().info("test");
                         for(Map.Entry<String, String> team : game_teams.entrySet()) {
                             int count = 0;
-                            for(Map.Entry<String, String> team_count : teams.get(game).entrySet()) {
+                            for(Map.Entry<String, String> team_count : game2.teams.entrySet()) {
                                 if(team_count.getKey().equals(team.getKey())) count++;
                             }
-                            if((count != Integer.parseInt(team.getValue()))&&(getWithValue(joined_players, game).size()>2)) {
+                            if((count != Integer.parseInt(team.getValue())) && (game2.joined_players.size() > 2)) {
                                 joinTeam(player, team.getKey());
                                 break;
                             } else if(count == 0) {
@@ -238,60 +245,62 @@ public class Game {
                         }
                     }
                 }
+                Bukkit.getLogger().info(getPlayerTeam("Lort533"));
             }
             for(Map<String, Object> entry : getTeams(game).values()) {
-                beds.put(((Location) entry.get("bed1")).getBlock().getType(), (Location) entry.get("bed1"));
-                beds.put(((Location) entry.get("bed2")).getBlock().getType(), (Location) entry.get("bed2"));
+                game2.beds.put(((Location) entry.get("bed1")).getBlock().getType(), (Location) entry.get("bed1"));
+                game2.beds.put(((Location) entry.get("bed2")).getBlock().getType(), (Location) entry.get("bed2"));
             }
-            for(String player : getWithValue(joined_players, game).keySet()) {
-                Bukkit.getPlayer(player).teleport((Location) getTeams(game).get(getPlayerTeam(game, player)).get("spawn"));
+            for(String player : game2.joined_players) {
+                Bukkit.getPlayer(player).teleport((Location) getTeams(game).get(getPlayerTeam(player)).get("spawn"));
             }
             for(Map<String, Object> shop : getTeams(game).values()) createNpc(game, (Location) shop.get("villager"));
             for(Map.Entry<String, Map<String, Object>> spawner : getSpawners(game).entrySet()) {
                 int id = Bukkit.getServer().getScheduler().scheduleAsyncRepeatingTask(new Main(), new Runnable() {
                     public void run() {
-                        if(!games.get(game)) return;
+                        if(getGame(game) == null) return;
                         Bukkit.getWorld(cfg.get(game, "world")).dropItem((Location) spawner.getValue().get("location"), new ItemStack(Material.getMaterial(String.valueOf(spawner.getValue().get("type"))), 1));
                     }
                 }, 0L, Integer.parseInt(String.valueOf(spawner.getValue().get("time"))) * 20L);
-                task_ids.put(id, game);
+                game2.task_ids.add(id);
             }
         }
 
         public static String endGame(String game) {
+            BWGame game2 = getGame(game);
+            if(game2 == null) return ChatColor.RED + "The given map is not running!";
             for(Map.Entry<String, Location> blocks : Block.blocks.entrySet()) {
                 if(blocks.getKey().equals(game)) blocks.getValue().getBlock().setType(Material.AIR);
             }
-            for(Map.Entry<Material, Location> bed : beds.entrySet()) {
+            for(Map.Entry<Material, Location> bed : game2.beds.entrySet()) {
                 Bukkit.getWorld(cfg.get(game, "world")).getBlockAt(bed.getValue()).setType(bed.getKey());
             }
-            Map<String, String> list = getWithValue(joined_players, game);
-            for(String player : list.keySet()) leaveGame(player);
-            HashMap<Integer, String> tasks_copy = new HashMap<>(task_ids);
-            for(Map.Entry<Integer, String> data : tasks_copy.entrySet()) {
-                if(data.getValue().equals(game)) {
-                    Bukkit.getScheduler().cancelTask(data.getKey());
-                    task_ids.remove(data.getKey());
-                }
+            ArrayList<String> joined_players_copy = new ArrayList<>(game2.joined_players);
+            for(String player : joined_players_copy) leaveGame(player);
+            ArrayList<Integer> tasks_copy = new ArrayList<>(game2.task_ids);
+            for(Integer data : tasks_copy) {
+                Bukkit.getScheduler().cancelTask(data);
+                game2.task_ids.remove(data);
             }
-            HashMap<Integer, Map.Entry<String, Location>> npcs_copy = new HashMap<>(npcs);
-            for(Map.Entry<Integer, Map.Entry<String, Location>> data : npcs_copy.entrySet()) {
+            HashMap<Integer, Location> npcs_copy = new HashMap<>(game2.npcs);
+            for(Map.Entry<Integer, Location> data : npcs_copy.entrySet()) {
                 try {
-                    if(data.getValue().getKey().equals(game)) {
-                        for(Entity e : data.getValue().getValue().getWorld().getEntities()) {
-                            if(e.getEntityId() == data.getKey()) e.remove();
-                        }
-                        npcs.remove(data.getKey());
+                    for(Entity e : data.getValue().getWorld().getEntities()) {
+                        if(e.getEntityId() == data.getKey()) e.remove();
                     }
+                    game2.npcs.remove(data.getKey());
                 } catch(Exception ignored) {}
             }
+            game2.destroyed_beds.clear();
+            game2.started = false;
             games.remove(game);
             return ChatColor.AQUA + "Map has been stopped successfully.";
         }
 
         public static void createNpc(String game, Location location) {
+            BWGame game2 = getGame(game);
             Sheep npc = (Sheep) Bukkit.getWorld(cfg.get(game, "world")).spawnCreature(location, CreatureType.SHEEP);
-            npcs.put(npc.getEntityId(), new AbstractMap.SimpleEntry<>(game, location));
+            game2.npcs.put(npc.getEntityId(), location);
             npc.setColor(DyeColor.GRAY);
             int id = Bukkit.getScheduler().scheduleAsyncRepeatingTask(Main.instance, new Runnable() {
                 @Override
@@ -299,7 +308,25 @@ public class Game {
                     npc.teleport(location);
                 }
             }, 0L, 1L);
-            task_ids.put(id, game);
+            game2.task_ids.add(id);
+        }
+        public static boolean isPlayerPlaying(String nick) {
+            for(Map.Entry<String, BWGame> data : games.entrySet()) {
+                if(data.getValue().joined_players.contains(nick)) return true;
+            }
+            return false;
+        }
+        public static String getPlayerMap(String nick) {
+            for(Map.Entry<String, BWGame> data : games.entrySet()) {
+                if(data.getValue().joined_players.contains(nick)) return data.getKey();
+            }
+            return null;
+        }
+        public static boolean isGameNPC(int id) {
+            for(Map.Entry<String, BWGame> data : games.entrySet()) {
+                if(data.getValue().npcs.containsKey(id)) return true;
+            }
+            return false;
         }
     }
 }
