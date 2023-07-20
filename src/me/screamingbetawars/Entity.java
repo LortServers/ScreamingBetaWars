6fa19732f7e8010ee724ee2841f83e19ce94ff93
Lort533 <76491869+Lort533@users.noreply.github.com>
2023-07-20 17:36:27 +0200

    Fixes and added features - credit in the description.
    You *may be able* to (incompletely) play the game now - from start to finish!
    
    Credit:
    https://www.spigotmc.org/threads/completely-prevent-a-physics-update.246613/#post-2464031
    https://bukkit.org/threads/placing-bed.168950/#post-1821479
    https://bukkit.org/threads/check-if-a-block-was-blown-up-by-tnt.27998/#post-507309
    https://bukkit.org/threads/how-to-properly-cancel-tnt-explosions.99106/#post-1321905
    https://bukkit.org/threads/how-to-properly-cancel-tnt-explosions.99106/#post-1322491
    https://www.tabnine.com/code/java/methods/org.bukkit.entity.Player/playSound
    https://bukkit.org/threads/jump-higher-shoot-player-in-the-air.21835/#post-397430
    https://bukkit.org/threads/gethandle-netserverhandler.119453/#post-1478610
    https://bukkit.org/threads/clear-armor.74293/#post-1204176

--- src/me/screamingbetawars/Entity.java
+++ src/me/screamingbetawars/Entity.java
@@ -6,6 +6,7 @@ import org.bukkit.Material;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerListener;
 import me.screamingbetawars.Main.EventHandler;
